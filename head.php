2bbfcf1d4fd814d4f8c6d4e22f041acecae5576c
Kenta1112 <64536281+Kenta1112@users.noreply.github.com>
2020-06-09 23:41:04 +0900

    Add files via upload

---
+++ head.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+  <script
+  src="https://code.jquery.com/jquery-3.5.1.js"
+  integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc="
+  crossorigin="anonymous"></script>
+  <title><?php echo $title;?>|ToDo</title>
+</head>
---
+++ signup.php
@@ -0,0 +1,123 @@
+<?php
+require('function.php');
+
+debug('「「「「「「「「「「「「「「「「「「「「「「「「「「「「');
+debug('ユーザ登録');
+debug('」」」」」」」」」」」」」」」」」」」」」」」」」」」」');
+debugLogStart();
+
+$err_msg=array();
+
+
+//POST送信ありの時
+if(!empty($_POST)){
+
+  $email=$_POST['email'];
+  $password=$_POST['password'];
+  $pass_retype=$_POST['pass_retype'];
+
+  //未入力チェック
+  validrequired($email,'email');
+  validrequired($password,'password');
+  validrequired($pass_retype,'pass_retype');
+
+  
+  if(empty($err_msg)){
+  //最大文字数チェック
+  validMaxLen($email,'email');
+  validMaxLen($password,'password');
+  validMaxLen($pass_retype,'pass_retype');
+  
+  if(empty($err_msg)){
+  //Email形式チェック
+  validemail($email,'email');
+  //パスワード一致チェック
+  validpasswordMatch($password,$pass_retype,'password');
+  //パスワード最小文字数チェック
+  validpasswordMin($password,'password');
+
+
+  if(empty($err_msg)){
+  //Emailアドレス重複チェック
+  validEmailMatch($email);
+  
+
+  if(empty($err_msg)){
+    //ユーザ登録
+    //例外処理
+    try{
+      //DB接続
+      $dbh=dbConnect();
+      //SQL文作成
+      $sql='INSERT INTO users (email,password,create_date,login_date) VALUE(:email,:password,:create_date,:login_date)';
+      $data=array(':email'=>$email,':password'=>password_hash($password,PASSWORD_DEFAULT),':create_date'=>date('Y-m-d H:i:s'),':login_date'=>date('Y-m-d H:i:s'));
+      $stmt=queryPost($dbh,$sql,$data);
+
+      if($stmt){
+        debug('ユーザ登録成功');
+
+        //セッション関係
+        //セッション有効期限(default=1hour)
+        $sessionLimit=60*60;
+        //ログイン日時
+        $_SESSION['login_date']=time();
+        debug('ユーザ登録日時'.print_r($_SESSION['login_date'],true));
+        //セッション有効期限
+        $_SESSION['login_limit']=$sessionLimit;
+        debug('セッション有効期限'.print_r($_SESSION['login_limit'],true));
+        //セッションのユーザーID
+        $_SESSION['user_id']=$dbh->lastInsertId();
+        debug('セッションのユーザーID'.print_r($_SESSION['user_id'],true));
+
+      }else{
+        debug('ユーザ登録失敗');
+        $err_msg['common']=MSG08;
+      }
+    }catch(Exception $e){
+      debug('DB接続エラー');
+      $err_msg['common']=MSG07;
+  }
+  }
+
+  }
+
+  }
+
+  }
+}
+
+
+?>
+
+
+<?php
+$title="ユーザ登録";
+require('head.php');
+?>
+
+
+<body class="site-width">
+  
+
+<section class="form">
+<h1>ユーザ登録</h1>
+<form action="" method="POST">
+<p><span class="err_msg"><?php err_msg('common');?></span></p>
+<p><span >Email</span><span class="err_msg"><?php err_msg('email');?></span></p>
+<input type="text" name="email">
+<p><span>パスワード</span><span class="err_msg"><?php err_msg('password');?></span></p>
+<input type="password" name="password">
+<p><span>パスワード再入力</span><span class="err_msg"><?php err_msg('pass_retype');?></span></p>
+<input type="password" name="pass_retype">
+<div class="btn_wrapper">
+<input type="submit" class="btn">
+</div>
+
+
+</form>
+</section>
+
+
+  
+</body>
+</html>
